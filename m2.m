b3d4f5a09d55f485e7885497871054404543e1c2
henrikesmaili <33199744+henrikesmaili@users.noreply.github.com>
2019-02-19 15:36:06 +0100

    Add files via upload
    antalet som presenteras i list måste adderas 1annars stämmer det ej

---
+++ m2.m
@@ -0,0 +1,55 @@
+function [list] = m2(lattice)
+%M1 Do block entropy by length one, return array with info
+%   Detailed explanation goes here
+numOfCombinations=16;
+list=zeros(numOfCombinations,4)-1;
+
+sweep=zeros(1,4);
+lengthOfLattice=length(lattice(:,1));
+stop=0;
+
+for i=1:lengthOfLattice
+    for j=1:lengthOfLattice
+       
+        sweep(1)=lattice(mod(i-1,lengthOfLattice)+1,mod(j+1,lengthOfLattice)+1);
+        sweep(2)=lattice(mod(i,lengthOfLattice)+1,mod(j+1,lengthOfLattice)+1);
+        sweep(3)=lattice(mod(i,lengthOfLattice)+1,mod(j-1,lengthOfLattice)+1);
+        sweep(4)=lattice(mod(i+1,lengthOfLattice)+1,mod(j-1,lengthOfLattice)+1);
+        
+        for k=1:length(sweep)
+            if sweep(k)==-1
+                sweep(k)=0;
+            end
+        end
+        x=lattice(mod(i,lengthOfLattice)+1,mod(j,lengthOfLattice)+1);
+        
+        d=bi2de(sweep,2);
+        
+        for k=1:numOfCombinations
+            if list(k,1)==-1 && stop==0
+                list(k,1)=d;
+                if x==1
+                    list(k,2)=list(k,2)+1;
+                else
+                    list(k,3)=list(k,3)+1;
+                end
+                list(k,4)=list(k,4)+1;
+                stop=1;
+            elseif list(k,1)==d && stop==0
+                if x==1
+                    list(k,2)=list(k,2)+1;
+                else
+                    list(k,3)=list(k,3)+1;
+                end
+                list(k,4)=list(k,4)+1;
+                stop=1;
+            end
+        end
+        stop=0;
+    end
+end
+
+
+
+end
+
